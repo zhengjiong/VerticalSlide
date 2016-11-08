7689e1c99b91cec1a22fb5660050525d90b67a33
zhengjiong <zhengjiong2006@gmail.com>
2016-11-08 22:24:51 +0800

    update

--- verticalSlide/src/main/java/com/stone/verticalslide/VerticalFragment1.java
+++ verticalSlide/src/main/java/com/stone/verticalslide/VerticalFragment1.java
@@ -9,6 +9,8 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,9 +19,11 @@ import java.util.List;
 
 public class VerticalFragment1 extends Fragment {
 
-	private CustomViewPager mViewPager;
+	private ViewPager mViewPager;
+	private CustomViewPager mGridViewPager;
     private CustScrollView mScrollView;
     private List<ImageView> mImageViewList = new ArrayList<>();
+    private List<GridView> mGridViewList = new ArrayList<>();
 
     @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,14 +39,80 @@ public class VerticalFragment1 extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        mViewPager = (CustomViewPager) view.findViewById(R.id.viewpager);
+        mViewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        mGridViewPager = (CustomViewPager) view.findViewById(R.id.gridviewpager);
 
-        for (int i = 0;i<3;i++) {
+        for (int i = 0;i<10;i++) {
             ImageView imageView = (ImageView) LayoutInflater.from(getActivity()).inflate(R.layout.image_layout, null);
             mImageViewList.add(imageView);
         }
 
         mViewPager.setAdapter(new ViewPagerAdapter());
+
+        for (int i = 0;i<20;i++) {
+            GridView gridView = new GridView(getContext());
+            gridView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            gridView.setColumnWidth(GridView.STRETCH_COLUMN_WIDTH);
+            gridView.setNumColumns(3);
+            gridView.setAdapter(new GridAdapter());
+            mGridViewList.add(gridView);
+        }
+
+        mGridViewPager.setAdapter(new GridViewPagerAdapter());
+    }
+
+    class GridAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 9;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                ImageView imageView = (ImageView) LayoutInflater.from(getActivity()).inflate(R.layout.image_layout, null);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setImageResource(R.drawable.capture01);
+                convertView = imageView;
+            }
+            return convertView;
+        }
+    }
+
+    class GridViewPagerAdapter extends PagerAdapter{
+
+        @Override
+        public int getCount() {
+            return mGridViewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(mGridViewList.get(position));
+            return mGridViewList.get(position);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            //super.destroyItem(container, position, object);
+            container.removeView(mGridViewList.get(position));
+        }
     }
 
     class ViewPagerAdapter extends PagerAdapter{
